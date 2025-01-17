c0d23a575b7025749c12439f84c7ff83f7860a7a
JABNIA <vaxojabnia@gmail.com>
2025-01-17 14:20:41 +0400

    GH-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Routes, Route, HashRouter } from 'react-router'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import HomePage from './HomePage'
 import Layout from './Layout'
 import Projects from './Projects'
@@ -12,16 +12,16 @@ function App() {
     <>
     <div>
       {/* <Background /> */}
-    <HashRouter>
+    <BrowserRouter basename='/jabnia-portfolio'>
       <Routes>
-        <Route path='/jabnia-portfolio/' element={<Layout />}>
+        <Route path='/jabnia-portfolio' element={<Layout />}>
           <Route index element={<HomePage />}/>
           <Route path='/jabnia-portfolio/projects' element={<Projects />}/>
           <Route path='/jabnia-portfolio/about' element={<About />}/>
           <Route path='/jabnia-portfolio/skills' element={<Skills />}/>
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
 
     </div>
     </>
