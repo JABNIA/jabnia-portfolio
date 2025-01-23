30e8cfc6b5ec914f24bf8feb4f479062ae776c82
JABNIA <106924455+JABNIA@users.noreply.github.com>
2025-01-23 11:59:08 +0400

    Update Projects.tsx

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -3,6 +3,9 @@ function Projects() {
   return (
     <div>
         Projects are Here
+        <div>
+          <a href="https://jabnia.github.io/audiophile">Audiophile site</a>
+        </div>
     </div>
   )
 }
